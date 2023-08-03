c3e4868f8d29a64f355089d9643ab82a591df53c
Alief Adha <88047810+lipzy13@users.noreply.github.com>
2023-08-03 19:50:13 +0700

    Time: 22 ms, Memory: 23.3 MB - LeetHub

--- reverse-string/reverse-string.cpp
+++ reverse-string/reverse-string.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int i=0;
-        int j=s.size()-1;
-        
-        while(i<=j){
-            int temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
-            i++;
-            j--;
-        }
+       int i = 0;
+       int j = s.size() - 1;
+       solve(s, i, j);
+    }
+    void solve(vector<char>& s, int i, int j){
+        if(i >= j) return;
+        swap(s[i],s[j]);
+        solve(s, ++i, --j);
     }
 };
