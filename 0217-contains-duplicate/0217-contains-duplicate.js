1485fcc9475fdd93aad33a0e96eb8fc7c3832a88
Alief Adha <88047810+lipzy13@users.noreply.github.com>
2023-08-06 04:08:55 +0700

    Time: 76 ms (94.69%), Space: 54.8 MB (42.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,10 +2,6 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = (nums, hashSet = new Set()) => {
-    for(const num of nums){
-        if(hashSet.has(num)) return true;
-        hashSet.add(num);
-    }
-    return false;
+var containsDuplicate = (nums, hashSet = new Set(nums)) => {
+    return nums.length > hashSet.size;
 }
